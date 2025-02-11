ecfac788055c8f46f97101025c75e1f0ade03987
perotin <34893315+lucasperotin@users.noreply.github.com>
2025-02-10 23:00:37 -0600

    Delete nersc directory

--- nersc/runner2stat.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-echo "plat,nodef,coref,memf,nodebwf,sysbwf,run,Number of Applications,Window Duration (s)"
-awk -v OFS=',' -v FS='[ -]+' '$1=="INFO:root:Window" && $3=="duration" {wd=$4} $1=="INFO:root:Window" && $3=="contains" {run=$13; sub(/.txt$/, "", run); print($7,$8,$9,$10,$11,$12,run,$4,wd)}' $*
